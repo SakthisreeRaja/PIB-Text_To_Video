57a6a5d803b3bfc0b442d8ed5c2f592adb343ff4
SakthisreeRaja <sakthisreeraja@gmail.com>
2025-11-17 22:41:13 +0530

    Complete UI without the translation part

--- frontend/PIB_Text_Video/src/App.jsx
+++ frontend/PIB_Text_Video/src/App.jsx
@@ -3,10 +3,30 @@ import './App.css'
 import PressList from './components/PressList.jsx';
 function App() {
 
-  const [category, setCategory] = useState('All');
+  const [category, setCategory] = useState('All Ministry');
   const [language, setLanguage] = useState('English');
-  const [fromYear, setFromYear] = useState('');
-  const [toYear, setToYear] = useState('');
+  const [day, setDay] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+
+  const [tempCategory, setTempCategory] = useState('All Ministry');
+  const [tempLanguage, setTempLanguage] = useState('English');
+  const [tempDay, setTempDay] = useState('');
+  const [tempMonth, setTempMonth] = useState('');
+  const [tempYear, setTempYear] = useState('');
+
+  const handleApply = () => {
+    setCategory(tempCategory);
+    setLanguage(tempLanguage);
+    setDay(tempDay);
+    setMonth(tempMonth);
+    setYear(tempYear);
+  };
+
+  const handleClear = () => {
+    setCategory('All Ministry'); setLanguage('English'); setDay(''); setMonth(''); setYear('');
+    setTempCategory('All Ministry'); setTempLanguage('English'); setTempDay(''); setTempMonth(''); setTempYear('');
+  };
 
   return (
     <div>
@@ -27,54 +47,174 @@ function App() {
         <PressList
           category={category}
           language={language}
-          fromYear={fromYear}
-          toYear={toYear} />
+          day={day}
+          month={month}
+          year={year}
+        />
         <aside className='sidebar'>
           <div className='filter-panel'>
             <div className='filter-group'>
-              <label className='filter-label'>Year Range</label>
-              <div className='year-inputs'>
+              <label className='filter-label'>Date</label>
+
+              <div className='date-row'>
                 <input
                   type='number'
-                  placeholder='From'
+                  placeholder='Date'
+                  min='1'
+                  max='31'
+                  value={tempDay}
+                  onChange={(e) => setTempDay(e.target.value)}
                   className='filter-input'
-                  min='1950'
-                  max='2050'
-                  value={fromYear}
-                  onChange={(e) => setFromYear(e.target.value)}
                 />
-                <span className='to-text'>to</span>
+
+                <select
+                  value={tempMonth}
+                  onChange={(e) => setTempMonth(e.target.value)}
+                  className='filter-input'
+                >
+                  <option value=''>Mon</option>
+
+                  <option value="January">Jan</option>
+                  <option value="February">Feb</option>
+                  <option value="March">Mar</option>
+                  <option value="April">Apr</option>
+                  <option value="May">May</option>
+                  <option value="June">Jun</option>
+                  <option value="July">Jul</option>
+                  <option value="August">Aug</option>
+                  <option value="September">Sep</option>
+                  <option value="October">Oct</option>
+                  <option value="November">Nov</option>
+                  <option value="December">Dec</option>
+                </select>
+
+
                 <input
                   type='number'
-                  placeholder='To'
-                  className='filter-input'
+                  placeholder='Year'
                   min='1950'
                   max='2050'
-                  value={toYear}
-                  onChange={(e) => setToYear(e.target.value)}
+                  value={tempYear}
+                  onChange={(e) => setTempYear(e.target.value)}
+                  className='filter-input'
                 />
               </div>
-              <label className='filter-label'>Category</label>
+
+              <label className='filter-label'>Ministry</label>
               <select
                 className='filter-input'
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
+                value={tempCategory}
+                onChange={(e) => setTempCategory(e.target.value)}
               >
-                <option>All</option>
-                <option>Agriculture</option>
-                <option>Technology</option>
-                <option>Governance</option>
-                <option>Defence</option>
-                <option>Environment</option>
-                <option>Science</option>
-                <option value="President's Secretariat">President's Secretariat</option>
-                <option value="Prime Minister's Office">Prime Minister's Office</option>
+                <option>All Ministry</option>
+                <option>President's Secretariat</option>
+                <option>Vice President's Secretariat</option>
+                <option>Prime Minister's Office</option>
+
+                <option>Lok Sabha Secretariat</option>
+                <option>Rajya Sabha Secretariat</option>
+
+                <option>Cabinet</option>
+                <option>Cabinet Committee Decisions</option>
+                <option>Cabinet Committee on Economic Affairs (CCEA)</option>
+                <option>Cabinet Secretariat</option>
+                <option>Cabinet Committee on Infrastructure</option>
+                <option>Cabinet Committee on Price</option>
+                <option>Cabinet Committee on Investment</option>
+                <option>Other Cabinet Committees</option>
+
+                <option>AYUSH</option>
+                <option>Department of Space</option>
+                <option>Department of Ocean Development</option>
+                <option>Department of Atomic Energy</option>
+                <option>Election Commission</option>
+                <option>Finance Commission</option>
+
+                <option>Ministry of Agriculture & Farmers Welfare</option>
+                <option>Ministry of Agro & Rural Industries</option>
+                <option>Ministry of Chemicals and Fertilizers</option>
+                <option>Department of Pharmaceuticals</option>
+                <option>Department of Fertilizers</option>
+                <option>Department of Chemicals and Petrochemicals</option>
+
+                <option>Ministry of Civil Aviation</option>
+                <option>Ministry of Coal</option>
+                <option>Ministry of Commerce & Industry</option>
+                <option>Ministry of Communications</option>
+                <option>Ministry of Company Affairs</option>
+                <option>Ministry of Consumer Affairs, Food & Public Distribution</option>
+                <option>Ministry of Cooperation</option>
+                <option>Ministry of Corporate Affairs</option>
+                <option>Ministry of Culture</option>
+                <option>Ministry of Defence</option>
+                <option>Ministry of Development of North-East Region</option>
+                <option>Ministry of Disinvestment</option>
+                <option>Ministry of Drinking Water & Sanitation</option>
+                <option>Ministry of Earth Sciences</option>
+                <option>Ministry of Education</option>
+                <option>Ministry of Electronics & IT</option>
+                <option>Ministry of Environment, Forest and Climate Change</option>
+                <option>Ministry of External Affairs</option>
+                <option>Ministry of Finance</option>
+                <option>Ministry of Fisheries, Animal Husbandry & Dairying</option>
+                <option>Ministry of Food Processing Industries</option>
+                <option>Ministry of Health and Family Welfare</option>
+                <option>Ministry of Heavy Industries</option>
+                <option>Ministry of Home Affairs</option>
+                <option>Ministry of Housing & Urban Affairs</option>
+                <option>Ministry of Information & Broadcasting</option>
+                <option>Ministry of Jal Shakti</option>
+                <option>Ministry of Labour & Employment</option>
+                <option>Ministry of Law and Justice</option>
+                <option>Ministry of Micro,Small & Medium Enterprises</option>
+                <option>Ministry of Mines</option>
+                <option>Ministry of Minority Affairs</option>
+                <option>Ministry of New and Renewable Energy</option>
+                <option>Ministry of Overseas Indian Affairs</option>
+                <option>Ministry of Panchayati Raj</option>
+                <option>Ministry of Parliamentary Affairs</option>
+                <option>Ministry of Personnel, Public Grievances & Pensions</option>
+                <option>Ministry of Petroleum & Natural Gas</option>
+                <option>Ministry of Planning</option>
+                <option>Ministry of Power</option>
+                <option>Ministry of Railways</option>
+                <option>Ministry of Road Transport & Highways</option>
+                <option>Ministry of Rural Development</option>
+                <option>Ministry of Science & Technology</option>
+                <option>Ministry of Ports, Shipping and Waterways</option>
+                <option>Ministry of Skill Development and Entrepreneurship</option>
+                <option>Ministry of Social Justice & Empowerment</option>
+                <option>Ministry of Statistics & Programme Implementation</option>
+                <option>Ministry of Steel</option>
+                <option>Ministry of Surface Transport</option>
+                <option>Ministry of Textiles</option>
+                <option>Ministry of Tourism</option>
+                <option>Ministry of Tribal Affairs</option>
+                <option>Ministry of Urban Development</option>
+                <option>Ministry of Water Resources, River Development and Ganga Rejuvenatior</option>
+                <option>Ministry of Women and Child Development</option>
+                <option>Ministry of Youth Affairs and Sports</option>
+                <option>NITI Aayog</option>
+                <option>PM Speech</option>
+                <option>EAC-PM</option>
+                <option>UPSC</option>
+                <option>Special Service and Features</option>
+                <option>PIB Headquarters</option>
+                <option>Office of Principal Scientific Advisor to Gol</option>
+                <option>National Financial Reporting Authority</option>
+                <option>Competition Commission of India</option>
+                <option>IFSC Authority</option>
+                <option>National Security Council Secretariat</option>
+                <option>National Human Rights Commission</option>
+                <option>Lokpal of India</option>
+                <option>home</option>
+                <option>ministry of parliamentary affairs</option>
               </select>
               <label className='filter-label'>Language</label>
               <select className='filter-input'
-                value={language}
-                onChange={(e) => setLanguage(e.target.value)}
-                >
+                value={tempLanguage}
+                onChange={(e) => setTempLanguage(e.target.value)}
+              >
                 <option>English</option>
                 <option>Hindi</option>
                 <option>Urdu</option>
@@ -91,15 +231,10 @@ function App() {
                 <option>Manipuri</option>
               </select>
               <div className='filter-buttons'>
-                <button className='apply-btn'>Apply</button>
+                <button className='apply-btn' onClick={handleApply}>Apply</button>
                 <button
                   className='clear-btn'
-                  onClick={() => {
-                    setCategory('All');
-                    setLanguage('English');
-                    setFromYear('');
-                    setToYear('');
-                  }}
+                  onClick={handleClear}
                 >
                   Clear
                 </button>
--- frontend/PIB_Text_Video/src/components/PressList.jsx
+++ frontend/PIB_Text_Video/src/components/PressList.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from 'react';
 import './PressList.css'
 import SearchBar from './Searchbar.jsx';
-import { FaRegPlayCircle } from 'react-icons/fa';
+import { FaRegPlayCircle, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import ImageCarousel from './ImageCarousel.jsx';
 import VideoModal from './VideoModal.jsx';
 
 const SAMPLE_VIDEO_URL = "https://www.w3schools.com/html/mov_bbb.mp4";
 const API_BASE_URL = "http://127.0.0.1:8000";
+const ITEMS_PER_PAGE = 9;
 
-function PressList({ category, language, fromYear, toYear }) {
+function PressList({ category, language, day, month, year }) {
     const [pressReleases, setPressReleases] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-
+    const [currentPage, setCurrentPage] = useState(1);
     const [SearchTerm, setSearchTerm] = useState('');
     const [playvideo, setPlayvideo] = useState(null);
 
@@ -25,32 +26,60 @@ function PressList({ category, language, fromYear, toYear }) {
                     throw new Error(`Failed to connect to the backend API`);
                 }
                 const data = await response.json();
-                setPressReleases(data);
+                if (Array.isArray(data)) {
+                    setPressReleases(data);
+                } else {
+                    console.error("Backend Error:", data);
+                    setError(data.error || "Invalid data received.");
+                }
                 setLoading(false);
             } catch (err) {
-                console.error("Erroe fetching data:", err);
+                console.error("Error fetching data:", err);
                 setError("Could not load press releases. Is the backend server running?");
                 setLoading(false);
             }
         };
 
         fetchData();
-    }, []);
+    }, []); 
 
-    const filteredReleases = pressReleases.filter((release) => {
+    const monthMap = {
+        "January": "JAN", "February": "FEB", "March": "MAR", "April": "APR",
+        "May": "MAY", "June": "JUN", "July": "JUL", "August": "AUG",
+        "September": "SEP", "October": "OCT", "November": "NOV", "December": "DEC"
+    };
+
+    const filteredReleases = Array.isArray(pressReleases) ? pressReleases.filter((release) => {
+        const matchesCategory = (category === 'All Ministry') || (release.ministry_name === category);
+
+        const dateParts = release.release_date ? release.release_date.split(" ") : [];
+        const releaseDay = dateParts.length > 0 ? parseInt(dateParts[0]) : null;
+        const releaseMonth = dateParts.length > 1 ? dateParts[1].toUpperCase() : "";
+        const releaseYear = release.release_year;
+
+        const matchesDay = !day || (releaseDay === parseInt(day));
+        const matchesMonth = !month || (releaseMonth === monthMap[month]);
+        const matchesYear = !year || (releaseYear === parseInt(year));
+
         const title = release.title || "";
-        const releaseCategory = release.ministry_name || "General";
-        const releaseYear = release.release_year || 0;
-
         const matchesSearch = title.toLowerCase().includes(SearchTerm.toLowerCase());
-        const matchesCategory = (category === 'All') || (releaseCategory === category);
 
-        const from = fromYear ? parseInt(fromYear) : 0;
-        const to = toYear ? parseInt(toYear) : 9999;
-        const matchesYear = releaseYear >= from && releaseYear <= to;
+        return matchesCategory && matchesDay && matchesMonth && matchesYear && matchesSearch;
+    }) : [];
 
-        return matchesSearch && matchesCategory && matchesYear;
+    const sortedReleases = filteredReleases.sort((a, b) => {
+        return new Date(b.release_date) - new Date(a.release_date);
     });
+
+    useEffect(() => {
+        setCurrentPage(1);
+    }, [category, language, day, month, year, SearchTerm]);
+
+    const indexOfLastItem = currentPage * ITEMS_PER_PAGE;
+    const indexOfFirstItem = indexOfLastItem - ITEMS_PER_PAGE;
+
+    const currentItems = sortedReleases.slice(indexOfFirstItem, indexOfLastItem);
+    const totalPages = Math.ceil(sortedReleases.length / ITEMS_PER_PAGE);
 
     const handleWatchVideo = (release) => {
         setPlayvideo(release);
@@ -59,8 +88,25 @@ function PressList({ category, language, fromYear, toYear }) {
     const handleCloseModal = () => {
         setPlayvideo(null);
     }
-    if (loading) return <div className="loading">Loading Press Releases...</div>;
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+        window.scrollTo({ top: 400, behavior: 'smooth' });
+    };
+
+    if (loading) {
+        return (
+            <div className="loading-container">
+                <div className="loading">
+                    <div className="spinner"></div>
+                    <p>Loading Press Releases...</p>
+                </div>
+            </div>
+        );
+    }
+
     if (error) return <div className="error-message">{error}</div>;
+
     return (
         <section className='press-list'>
             <div className='press-header'>
@@ -72,8 +118,8 @@ function PressList({ category, language, fromYear, toYear }) {
                 <SearchBar setSearchTerm={setSearchTerm} />
             </div>
             <div className='cards'>
-                {filteredReleases.length > 0 ? (
-                    filteredReleases.map((release) => (
+                {currentItems.length > 0 ? (
+                    currentItems.map((release) => (
                         <div key={release.id} className="card">
                             <h3>{release.title}</h3>
                             <p>{release.release_date}</p>
@@ -87,6 +133,29 @@ function PressList({ category, language, fromYear, toYear }) {
                     <div className='no-results'><p>No press release found.</p></div>
                 )}
             </div>
+            {sortedReleases.length > ITEMS_PER_PAGE && (
+                <div className="pagination">
+                    <button
+                        onClick={() => paginate(currentPage - 1)}
+                        disabled={currentPage === 1}
+                        className="page-btn"
+                    >
+                        <FaChevronLeft /> Prev
+                    </button>
+
+                    <span className="page-info">
+                        Page {currentPage} of {totalPages}
+                    </span>
+
+                    <button
+                        onClick={() => paginate(currentPage + 1)}
+                        disabled={currentPage === totalPages}
+                        className="page-btn"
+                    >
+                        Next <FaChevronRight />
+                    </button>
+                </div>
+            )}
             {playvideo && (
                 <VideoModal
                     releaseData={playvideo}
@@ -96,8 +165,6 @@ function PressList({ category, language, fromYear, toYear }) {
             )}
         </section>
     );
-
-
 }
 
 export default PressList;
